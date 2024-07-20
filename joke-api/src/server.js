b6c5f9b64a2c017dfaa1cc92d2510e4af4b86fe2
shehanreshin <asp.shehanreshin@gmail.com>
2024-07-20 13:57:30 +0530

    Create an endpoint to get a sample joke for testing purposes

--- joke-api/src/server.js
+++ joke-api/src/server.js
@@ -6,6 +6,21 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  joke = {
+    id: 20,
+    category: "salty",
+    content: {
+      line1: "Line 1 of joke",
+      line2: "Line 2 of joke",
+      line3: "Line 3 of joke",
+    },
+    author: "Jian Yang",
+    date_created: Date(Date.now()),
+  };
+  res.status(200).send(joke);
+});
+
 app.listen(PORT, () => {
   console.log(`Joke API listening at http://localhost:${PORT}`);
 });
